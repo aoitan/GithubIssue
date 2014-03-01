e5c9b023ecd57be2ae33dddc9d2090d123508ae3
あおいたん <aoi@nagatsuki-do.net>
2014-03-01 20:28:47 +0900

    aoitan/anything#26 aoitan/anything#30 aoitan/anything#31 aoitan/anything#34 aoitan/anything#37 UI定義

--- src/net/nagatsuki_do/android/githubissue/MainActivity.java
+++ src/net/nagatsuki_do/android/githubissue/MainActivity.java
@@ -1,6 +1,7 @@
 package net.nagatsuki_do.android.githubissue;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,6 +10,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		Intent intent = new Intent(MainActivity.this, PostActivity.class);
+		startActivity(intent);
+		finish();
 	}
 
 }
--- src/net/nagatsuki_do/android/githubissue/PostActivity.java
+++ src/net/nagatsuki_do/android/githubissue/PostActivity.java
@@ -1,7 +1,15 @@
 package net.nagatsuki_do.android.githubissue;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class PostActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.post_activity);
+	}
 
 }
